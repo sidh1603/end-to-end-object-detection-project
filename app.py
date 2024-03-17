9ceba01e7a871612a2edaeae8490a3fbed49fa1f
sid <sidhu.rana34@gmail.com>
2024-03-16 19:19:21 -0500

    notebook added

---
+++ app.py
@@ -0,0 +1,14 @@
+import logging ,os,sys
+
+from signLanguage.logger import logging
+from signLanguage.exception import SignException
+import sys
+
+logging.info("welcome to the project")
+
+try:
+
+    a=7/'9'
+
+except Exception as e:
+    raise SignException(e, sys) from e
