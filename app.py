af601ad8e5b6d2b5f022fbea9966f4bd666a8f77
sid <sidhu.rana34@gmail.com>
2024-03-18 22:25:39 -0500

    added

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-import logging ,os,sys
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
-import sys
+obj = TrainPipeline()
+obj.run_pipeline()
 
-logging.info("welcome to the project")
-
-try:
-
-    a=7/'9'
-
-except Exception as e:
-    raise SignException(e, sys) from e
--- signLanguage/pipeline/training_pipeline.py
+++ signLanguage/pipeline/training_pipeline.py
@@ -120,7 +120,8 @@ class TrainPipeline:
 
             if data_validation_artifact.validation_status == True:
                 model_trainer_artifact = self.start_model_trainer()
-                model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact,s3=self.s3_operations)
+                model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact, s3=self.s3_operations)
+
 
             else:
                 raise Exception("Your data is not in correct format")
